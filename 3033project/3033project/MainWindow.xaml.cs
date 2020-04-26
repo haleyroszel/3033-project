4f8d9b658fbe047342a0b10956b3f67848fb3fd5
Haley Roszel <roszel@ou.edu>
2020-04-25 20:03:39 -0500

    restart

--- 3033project/3033project/MainWindow.xaml.cs
+++ 3033project/3033project/MainWindow.xaml.cs
@@ -25,22 +25,106 @@ namespace _3033project
             InitializeComponent();
         }
 
-        private void btnPreviousTab_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //https://www.wpf-tutorial.com/tabcontrol/using-the-tabcontrol/
-            int newIndex = Quiz.SelectedIndex - 1;
-            if (newIndex < 0)
-                newIndex = Quiz.Items.Count - 1;
-            Quiz.SelectedIndex = newIndex;
+            quiz res = new quiz();
+            res.setFruit(1);
         }
 
-        private void btnNextTab_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-        //https://www.wpf-tutorial.com/tabcontrol/using-the-tabcontrol/
-            int newIndex = Quiz.SelectedIndex + 1;
-            if (newIndex >= Quiz.Items.Count)
-                newIndex = 0;
-            Quiz.SelectedIndex = newIndex;
+            quiz res = new quiz();
+            res.setFruit(2);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setFruit(3);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setFruit(4);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setActivity(1);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setActivity(2);
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setActivity(3);
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setActivity(4);
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setRoof(1);
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setRoof(2);
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setRoof(3);
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setRoof(4);
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setWorker(1);
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setWorker(2);
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setWorker(3);
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+            quiz res = new quiz();
+            res.setWorker(4);
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+
--- 3033project/3033project/quiz.cs
+++ 3033project/3033project/quiz.cs
@@ -8,65 +8,86 @@ namespace _3033project
 {
     class quiz
     {
-        public int Fruit { get; set; }
-        public int Activity { get; set; }
-        public int Slider { get; set; }
-        public int Furniture { get; set; }
-        public int Worker { get; set; }
-        public int Roof { get; set; }
-        public int Style { get; set; }
-        public int Constellation { get; set; }
-        public int Museum { get; set; }
-        public int Tool { get; set; }
+        private int Fruit;
+        private int Activity;
+        private int Roof;
+        private int Worker;
+
+        public int getFruit()
+        {
+            return Fruit;
+        }
+        public void setFruit(int newFruit)
+        {
+            Fruit = newFruit;
+        }
+
+        public int getActivity()
+        {
+            return Activity;
+        }
+        public void setActivity(int newActivity)
+        {
+            Activity = newActivity;
+        }
+
+        public int getRoof()
+        {
+            return Roof;
+        }
+        public void setRoof(int newRoof)
+        {
+            Roof = newRoof;
+        }
+
+
+        public int getWorker()
+        {
+            return Worker;
+        }
+        public void setWorker(int newWorker)
+        {
+            Worker = newWorker;
+        }
 
         public quiz()
         {
-
             Fruit = 0;
             Activity = 0;
-            Slider = 0;
-            Furniture = 0;
-            Worker = 0;
             Roof = 0;
-            Style = 0;
-            Constellation = 0;
-            Museum = 0;
-            Tool = 0;
+            Worker = 0;
         }
-
-        public string Results ()
+        public string Results()
         {
             string character = string.Empty;
             double total = 0;
-            total = Fruit + Activity + Slider + Furniture + Worker + Roof + Style + Constellation + Museum + Tool;
+            total = Fruit + Activity + Worker + Roof;
 
-            if (total >= 35)
+            if (total >= 14)
             {
                 character = "Rover";
             }
-            else if (total >= 30)
+            else if (total >= 12)
             {
                 character = "Skye";
             }
-            else if (total >= 25)
+            else if (total >= 10)
             {
                 character = "Octavian";
             }
-            else if (total >= 20)
+            else if (total >= 8)
             {
                 character = "Goldie";
             }
-            else if (total >= 15)
+            else if (total >= 6)
             {
                 character = "Lobo";
             }
-            else if (total >= 10)
+            else if (total >= 4)
             {
                 character = "Cookie";
             }
             return character;
         }
     }
-
-    
 }
